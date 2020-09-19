7b5b2eedb5eab393d4c5ecb34035c0d77cdaae3b
kzchan <kzchan0816@qq.com>
2020-09-19 08:48:04 +0800

    封装三个页面布局

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,10 +1,7 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
-      Labels.vue
-    </div>
-    <Nav/>
-  </div>
+  <Layout>
+    Labels.vue
+  </Layout>
 </template>
 
 <script lang="ts">
@@ -12,16 +9,3 @@
     name: 'Labels',
   };
 </script>
-
-<style lang="scss" scoped>
-  .nav-wrapper{
-    border: 1px solid green;
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
-  }
-  .content{
-    flex-grow: 1;
-    overflow: auto;
-  }
-</style>
