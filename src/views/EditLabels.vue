a3cbc80f890b688fbe47d5b46bc06b831bd608b3
kzchan <kzchan0816@qq.com>
2020-09-24 20:48:46 +0800

    展示 tag 的name

--- src/views/EditLabels.vue
+++ src/views/EditLabels.vue
@@ -6,7 +6,7 @@
       <span class="rightIcon"></span>
     </div>
     <div class="form-wrapper">
-      <FormItem field-name="标签名" placeholder="请输入标签名"/>
+      <FormItem :value="tag.name" field-name="标签名" placeholder="请输入标签名"/>
     </div>
     <div class="button-wrapper">
       <Button>删除标签</Button>
@@ -20,18 +20,21 @@
   import tagListModel from '@/models/tagListModel';
   import FormItem from '@/components/Money/FormItem.vue';
   import Button from '@/components/Button.vue';
+
   @Component({
     components: {Button, FormItem}
   })
   export default class EditLabels extends Vue {
-    created(){
+    tag?: { id: string; name: string } = undefined;
+
+    created() {
       const id = this.$route.params.id;
       tagListModel.fetch();
       const tags = tagListModel.data;
-      const tag = tags.filter(t => t.id === id)[0]
-      if (tag){
-        console.log(tag);
-      }else {
+      const tag = tags.filter(t => t.id === id)[0];
+      if (tag) {
+        this.tag = tag;
+      } else {
         this.$router.replace('/404');
       }
     }
@@ -39,7 +42,7 @@
 </script>
 
 <style lang="scss" scoped>
-  .navBar{
+  .navBar {
     text-align: center;
     font-size: 16px;
     padding: 12px 16px;
@@ -47,23 +50,28 @@
     display: flex;
     align-items: center;
     justify-content: space-between;
-    >.title{
+
+    > .title {
 
     }
-    >.leftIcon{
+
+    > .leftIcon {
       width: 24px;
       height: 24px;
     }
-    >.rightIcon{
+
+    > .rightIcon {
       width: 24px;
       height: 24px;
     }
   }
-  .form-wrapper{
+
+  .form-wrapper {
     background: white;
     margin-top: 8px;
   }
-  .button-wrapper{
+
+  .button-wrapper {
     text-align: center;
     padding: 16px;
     margin-top: 44-16px;
