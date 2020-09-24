3ba61ecedd4efda9586b8bb40ab7c7e3a94bc783
kzchan <kzchan0816@qq.com>
2020-09-24 20:28:40 +0800

    添加EditLabel.vue 的样式，抽离了两个组件Button和FormItem

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <label class="notes">
+    <label class="formItem">
       {{value}}
       <span class="name">{{this.fieldName}}</span>
       <input type="text"
@@ -15,7 +15,7 @@
   import {Component, Prop, Watch} from 'vue-property-decorator';
 
   @Component
-  export default class Notes extends Vue {
+  export default class FormItem extends Vue {
     value = '';
 
     @Prop({required: true}) fieldName!: string;
@@ -30,9 +30,8 @@
 </script>
 
 <style lang="scss" scoped>
-  .notes {
+  .formItem {
     font-size: 14px;
-    background: #f5f5f5;
     padding-left: 16px;
     display: flex;
     align-items: center;
@@ -42,7 +41,7 @@
     }
 
     input {
-      height: 64px;
+      height: 40px;
       flex-grow: 1;
       background: transparent;
       border: none;
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,9 +2,11 @@
   <Layout class-prefix="layout">
     <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
-    <Notes field-name="备注"
-           placeholder="请在这里输入备注"
-           @update:value="onUpdateNotes"/>
+    <div class="notes">
+      <FormItem field-name="备注"
+                placeholder="请在这里输入备注"
+                @update:value="onUpdateNotes"/>
+    </div>
     <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
   </Layout>
 </template>
@@ -12,7 +14,7 @@
 <script lang="ts">
   import NumberPad from '@/components/Money/NumberPad.vue';
   import Types from '@/components/Money/Types.vue';
-  import Notes from '@/components/Money/Notes.vue';
+  import FormItem from '@/components/Money/FormItem.vue';
   import Tags from '@/components/Money/Tags.vue';
   import Vue from 'vue';
   import {Component, Watch} from 'vue-property-decorator';
@@ -24,7 +26,7 @@
 
 
   @Component({
-    components: {Tags, Notes, Types, NumberPad},
+    components: {Tags, FormItem, Types, NumberPad},
   })
   export default class Money extends Vue {
     tags = tagList;
@@ -61,5 +63,8 @@
   .layout-content {
     display: flex;
     flex-direction: column-reverse;
+  }
+  .notes{
+    padding: 12px 0;
   }
 </style>
