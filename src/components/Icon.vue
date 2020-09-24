3249b6392547d17e300ce7532e9ee36988e41bc7
kzchan <kzchan0816@qq.com>
2020-09-24 21:34:58 +0800

    实现编辑和删除标签

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg class="icon">
+  <svg class="icon" @click="$emit('click', $event)">
     <use v-bind:xlink:href="'#'+ name"></use>
   </svg>
 </template>
@@ -8,18 +8,19 @@
   const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
   try {
     importAll(require.context('../assets/icons', true, /\.svg$/));
-  }catch (error) {
+  } catch (error) {
     console.log(error);
   }
   export default {
-    props:['name'],
+    props: ['name'],
     name: 'Icon'
   };
 </script>
 
 <style lang="scss" scoped>
-  .icon{
-    width: 1em; height: 1em;
+  .icon {
+    width: 1em;
+    height: 1em;
     vertical-align: -0.15em;
     fill: currentColor;
     overflow: hidden;
