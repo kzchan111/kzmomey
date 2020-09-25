e403b5c3f57d2ac3cf978c0009bb3e0af04d72e6
kzchan <kzchan0816@qq.com>
2020-09-25 16:22:25 +0800

    封装tagList的增删改查

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -17,22 +17,15 @@
 <script lang="ts">
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
-  import tagListModel from '@/models/tagListModel';
 
-  tagListModel.fetch();
   @Component
   export default class Labels extends Vue {
-    tags = tagListModel.data;
+    tags = window.tagList;
 
     createTag() {
       const name = window.prompt('请输入标签名');
-      if (name) {
-        const message = tagListModel.create(name);
-        if (message === 'duplicated') {
-          window.alert('标签名重复');
-        } else if (message === 'success') {
-          window.alert('添加成功');
-        }
+      if (name){
+        window.createTag(name);
       }
     }
   }
